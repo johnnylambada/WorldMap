24200f22ad56245f5d09d0cdb07fa9ca3590a6c6
John Lombardo <john.lombardo@gmail.com>
2012-04-12 09:22:49 -0700

    Hide the window title

--- src/com/sigseg/android/worldmap/WorldMapActivity.java
+++ src/com/sigseg/android/worldmap/WorldMapActivity.java
@@ -2,12 +2,17 @@ package com.sigseg.android.worldmap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class WorldMapActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Hide the window title.
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
     }
 }
