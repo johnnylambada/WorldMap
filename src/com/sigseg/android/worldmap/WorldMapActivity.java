a582f6dc7779d01c5fd64ef9179dd2ad103da501
John Lombardo <John@Scopely.com>
2013-03-04 11:44:14 -0800

    Reformat code

--- src/com/sigseg/android/worldmap/WorldMapActivity.java
+++ src/com/sigseg/android/worldmap/WorldMapActivity.java
@@ -29,56 +29,54 @@ public class WorldMapActivity extends Activity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
         worldView = (WorldView) findViewById(R.id.worldview);
-        if (savedInstanceState!=null && savedInstanceState.containsKey(KEY_X) && savedInstanceState.containsKey(KEY_Y)){
-    		int x = (Integer) savedInstanceState.get(KEY_X);
-        	int y = (Integer) savedInstanceState.get(KEY_Y);
-		String fn;
-        	Point p = new Point(x,y);
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_X) && savedInstanceState.containsKey(KEY_Y)) {
+            int x = (Integer) savedInstanceState.get(KEY_X);
+            int y = (Integer) savedInstanceState.get(KEY_Y);
+            String fn;
+            Point p = new Point(x, y);
 
-		Log.d(TAG,"restoring state");
-		if (savedInstanceState.containsKey(KEY_FN)) 
-		    fn= (String) savedInstanceState.get(KEY_FN);
-		else fn=null;
-		Log.d(TAG,".. fn = "+fn);
-		try{
-		    if (fn=="" || fn==null){
-			Log.d(TAG,"restore, setting stream to world.jpg");
-			worldView.setImageIS(getAssets().open("world.jpg"));
-		    }
-		    else {
-			Log.d(TAG,"restore, opening file "+fn);
-			worldView.setImageIS(new RandomAccessFileInputStream(fn));
-		    }
-		    worldView.setViewport(p);
-		}catch (java.io.IOException e) {
-		    Log.e(TAG, e.getMessage());
-		}
-		Log.d(TAG,"restored state");
+            Log.d(TAG, "restoring state");
+            if (savedInstanceState.containsKey(KEY_FN))
+                fn = (String) savedInstanceState.get(KEY_FN);
+            else fn = null;
+            Log.d(TAG, ".. fn = " + fn);
+            try {
+                if (fn == "" || fn == null) {
+                    Log.d(TAG, "restore, setting stream to world.jpg");
+                    worldView.setImageIS(getAssets().open("world.jpg"));
+                } else {
+                    Log.d(TAG, "restore, opening file " + fn);
+                    worldView.setImageIS(new RandomAccessFileInputStream(fn));
+                }
+                worldView.setViewport(p);
+            } catch (java.io.IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+            Log.d(TAG, "restored state");
         } else {
-           	// Centering the map to start
-		try {
-		        Uri uri = getIntent() != null ? getIntent().getData() : null;
-			InputStream is;
-			if (uri != null ) {
-			    Log.d(TAG,"file is: "+uri.getPath());
-			    filename=uri.getPath();
-			    is = (InputStream) new RandomAccessFileInputStream(uri.getPath());
-			}
-			else {
-			    is = getAssets().open("world.jpg");
-			    filename="";
-			}
+            // Centering the map to start
+            try {
+                Uri uri = getIntent() != null ? getIntent().getData() : null;
+                InputStream is;
+                if (uri != null) {
+                    Log.d(TAG, "file is: " + uri.getPath());
+                    filename = uri.getPath();
+                    is = (InputStream) new RandomAccessFileInputStream(uri.getPath());
+                } else {
+                    is = getAssets().open("world.jpg");
+                    filename = "";
+                }
 
-			worldView.setImageIS(is);
-			Log.d(TAG,"filename is "+filename);
-		} catch (java.io.IOException e) {
-			Log.e(TAG, e.getMessage());
-		}
-        	worldView.setViewportCenter();
+                worldView.setImageIS(is);
+                Log.d(TAG, "filename is " + filename);
+            } catch (java.io.IOException e) {
+                Log.e(TAG, e.getMessage());
+            }
+            worldView.setViewportCenter();
         }
     }
 
-	@Override
+    @Override
 	protected void onSaveInstanceState(Bundle outState) {
 		Log.d(TAG, "onSaveInstanceState() filename "+filename);
 		Point p = worldView.getViewport();
