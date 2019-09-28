5c2dfe336aa3f59a03913daceb006430563147b2
John Lombardo <john@lombardos.org>
2019-09-28 16:32:26 -0700

    Simplify further

--- v3/app/src/main/java/com/sigseg/android/map/ImageViewerActivity.java
+++ v3/app/src/main/java/com/sigseg/android/map/ImageViewerActivity.java
@@ -14,47 +14,49 @@ public class ImageViewerActivity extends Activity {
     private static final String TAG = "ImageViewerActivity";
     private static final String KEY_X = "X";
     private static final String KEY_Y = "Y";
-
     private static final String MAP_FILE = "world.jpg";
     
     private ImageSurfaceView imageSurfaceView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Hide the window title.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.main);
-        imageSurfaceView = (ImageSurfaceView) findViewById(R.id.worldview);
+        imageSurfaceView = findViewById(R.id.worldview);
 
-        // Setup/restore state
+        try {
+            imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
+        } catch (java.io.IOException e) {
+            Log.wtf(TAG, "Fail", e);
+        }
+
         if (savedInstanceState != null && savedInstanceState.containsKey(KEY_X) && savedInstanceState.containsKey(KEY_Y)) {
-            Log.d(TAG, "restoring state");
-            int x = (int) savedInstanceState.get(KEY_X);
-            int y = (int) savedInstanceState.get(KEY_Y);
-
-            try {
-                imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
-                imageSurfaceView.setViewport(new Point(x, y));
-            } catch (java.io.IOException e) {
-                Log.e(TAG, e.getMessage());
-            }
+            imageSurfaceView.post(new Runnable() {
+                @Override
+                public void run() {
+                    final int x = (int) savedInstanceState.get(KEY_X);
+                    final int y = (int) savedInstanceState.get(KEY_Y);
+                    imageSurfaceView.setViewport(new Point(x, y));
+                }
+            });
         } else {
             // Centering the map to start
-            try {
-                imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
-                imageSurfaceView.setViewportCenter();
-            } catch (java.io.IOException e) {
-                Log.e(TAG, e.getMessage());
-            }
+            imageSurfaceView.post(new Runnable() {
+                @Override
+                public void run() {
+                    imageSurfaceView.setViewportCenter();
+                }
+            });
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        Point p = new Point();
+        final Point p = new Point();
         imageSurfaceView.getViewport(p);
         outState.putInt(KEY_X, p.x);
         outState.putInt(KEY_Y, p.y);
