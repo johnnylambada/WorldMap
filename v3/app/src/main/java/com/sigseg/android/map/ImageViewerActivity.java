a1b8fd8fad04f229f1d42022395b14ba417acfba
John Lombardo <john@lombardos.org>
2019-09-28 16:14:51 -0700

    simplify

--- v3/app/src/main/java/com/sigseg/android/map/ImageViewerActivity.java
+++ v3/app/src/main/java/com/sigseg/android/map/ImageViewerActivity.java
@@ -1,30 +1,23 @@
 package com.sigseg.android.map;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.graphics.Point;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.sigseg.android.io.RandomAccessFileInputStream;
 import com.sigseg.android.worldmap.R;
-
-import java.io.InputStream;
 
 
 public class ImageViewerActivity extends Activity {
     private static final String TAG = "ImageViewerActivity";
     private static final String KEY_X = "X";
     private static final String KEY_Y = "Y";
-    private static final String KEY_FN = "FN";
 
     private static final String MAP_FILE = "world.jpg";
     
     private ImageSurfaceView imageSurfaceView;
-    private String filename = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,44 +32,23 @@ public class ImageViewerActivity extends Activity {
         // Setup/restore state
         if (savedInstanceState != null && savedInstanceState.containsKey(KEY_X) && savedInstanceState.containsKey(KEY_Y)) {
             Log.d(TAG, "restoring state");
-            int x = (Integer) savedInstanceState.get(KEY_X);
-            int y = (Integer) savedInstanceState.get(KEY_Y);
-
-            String fn = null;
-            if (savedInstanceState.containsKey(KEY_FN))
-                fn = (String) savedInstanceState.get(KEY_FN);
+            int x = (int) savedInstanceState.get(KEY_X);
+            int y = (int) savedInstanceState.get(KEY_Y);
 
             try {
-                if (fn == null || fn.length()==0) {
-                    imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
-                } else {
-                    imageSurfaceView.setInputStream(new RandomAccessFileInputStream(fn));
-                }
+                imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
                 imageSurfaceView.setViewport(new Point(x, y));
             } catch (java.io.IOException e) {
                 Log.e(TAG, e.getMessage());
             }
         } else {
             // Centering the map to start
-            Intent intent = getIntent();
             try {
-                Uri uri = null;
-                if (intent!=null)
-                    uri = getIntent().getData();
-
-                InputStream is;
-                if (uri != null) {
-                    filename = uri.getPath();
-                    is = new RandomAccessFileInputStream(uri.getPath());
-                } else {
-                    is = getAssets().open(MAP_FILE);
-                }
-
-                imageSurfaceView.setInputStream(is);
+                imageSurfaceView.setInputStream(getAssets().open(MAP_FILE));
+                imageSurfaceView.setViewportCenter();
             } catch (java.io.IOException e) {
                 Log.e(TAG, e.getMessage());
             }
-            imageSurfaceView.setViewportCenter();
         }
     }
 
@@ -86,8 +58,6 @@ public class ImageViewerActivity extends Activity {
         imageSurfaceView.getViewport(p);
         outState.putInt(KEY_X, p.x);
         outState.putInt(KEY_Y, p.y);
-        if (filename!=null)
-            outState.putString(KEY_FN, filename);
         super.onSaveInstanceState(outState);
     }
 }
